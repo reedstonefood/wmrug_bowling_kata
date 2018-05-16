83cf218fc1a050c783d36a71df2983397d2877ff
Adre Du Toit <adre.dutoit@foxsoft.net>
2018-05-16 19:41:20 +0100

    refactor specs

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,18 +2,21 @@ require_relative '../game.rb'
 
 describe Game do
   it "gutter game should score zero" do
-    20.times do
-      subject.roll(0)
-    end
-    
+    roll_many( 20, 0 )
+
     expect(subject.score).to be 0
   end
 
   it "rolling 1 20 times should score 20" do
-    20.times do
-      subject.roll(1)
-    end
+    roll_many( 20, 1 )
     
     expect(subject.score).to be 20
+  end
+
+  private
+  def roll_many( rolls, pins)
+    rolls.times do
+      subject.roll(pins)
+    end
   end
 end
